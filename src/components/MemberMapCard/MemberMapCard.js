75ab8c855f894d5fd70166d5d63ced8f82ebea31
George Dilthey <george.dilthey@gmail.com>
2021-11-06 00:57:13 -0400

    added data to cards

--- src/components/MemberMapCard/MemberMapCard.js
+++ src/components/MemberMapCard/MemberMapCard.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
+import './MemberMapCard.css'
 
 
 const MemberMapCard = ({ hoveredStateName, members }) => {
    
   const member = members.filter(m => m.state_name === hoveredStateName) 
-
+  
   return (
     <div>
-      <h1>{member[0].first_name} {member[0].last_name}</h1>
-      <h1>{member[1].first_name} {member[1].last_name}</h1>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <img src={member[0].image_225x275}></img>
+            <h4>{member[0].short_title} {member[0].first_name} {member[0].last_name} ({member[0].party})</h4>
+            </div>
+          </div>
+        <div class="column">
+          <div class="card">
+            <img src={member[1].image_225x275}></img>
+            <h4>{member[1].short_title} {member[1].first_name} {member[1].last_name} ({member[1].party})</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
