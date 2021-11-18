0f1b5c4cb2b404b6da8ef4134bdfae359ad027b4
George Dilthey <george.dilthey@gmail.com>
2021-11-17 21:06:16 -0500

    Updated card styling

--- src/components/MemberCard/MemberCard.js
+++ src/components/MemberCard/MemberCard.js
@@ -2,20 +2,52 @@ import React from "react";
 import { connect } from "react-redux";
 import './MemberCard.css'
 import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Divider from '@mui/material/Divider';
+
+
 
 const MemberCard = ({ member, r }) => {
    
   return (
     <div className="column">
-      <Link to={`/members/${member.mid}`}>
-        <div className="card">
-          <img src={member.image225x275} alt={`${member.firstName} ${member.lastName}`} onError={(e)=>{
-            e.target.onerror = null; 
-            e.target.src=`${process.env.PUBLIC_URL}/assets/images/portrait-placeholder-grey.gif`
-            e.target.className = "placeholderImage"}}></img>
+      <Card sx={{textAlign: 'center', marginBottom: 2}}>
+        <CardContent>
+          <Avatar
+            alt={`${member.firstName} ${member.lastName}`}
+            src={member.image225x275}
+            sx={{ width: 100, height: 100, margin: "auto" }}
+          />
           <h4>{member.shortTitle} {member.firstName} {member.lastName} ({r.party})</h4>
+          <Link to={''} style={{textDecoration: 'none'}}><Button variant="outlined" size="small" sx={{ m: 1, width: 150 }}>Follow</Button></Link>
+          <Link to={`/members/${member.mid}`} style={{textDecoration: 'none'}}><Button variant="contained" size="small" sx={{width: 150, textDecoration: "none" }}>Learn More</Button></Link>
+        </CardContent>
+      <Divider light />
+      </Card>
+      {/* 
+        <Box sx={{ display: 'grid', gridTemplateRows: 'repeat(3, 1fr)', textAlign: "center" }}>
+          
+          
+          
+        
+          <h4>{member.shortTitle} {member.firstName} {member.lastName} ({r.party})</h4>
+        
+        
+          
+          
+        </Box>
+        <div className="card">
+          
+          <div>
+            
+          </div>
+          
         </div>
-      </Link>
+      </Link> */}
+      
     </div>  
   );
 };
